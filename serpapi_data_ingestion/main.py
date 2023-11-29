586e0ab4894b8fa7c40043dd2a23db7fe714a41c
Harsh Shah <shah.harsh7@northeastern.edu>
2023-11-28 23:00:00 -0500

    snowflake connection and EDA

--- serpapi_data_ingestion/main.py
+++ serpapi_data_ingestion/main.py
@@ -6,7 +6,6 @@ from utils import get_cleaned_data
 
 
 
-
 data = get_cleaned_data()
 
 def get_gmap_id(selected_name):
@@ -40,10 +39,49 @@ def get_reviews(restaurant_name):
     restaurant_name = raw_reviews_data['place_info']['title']
     snippets = ' '.join([review['snippet'] for review in raw_reviews_data['reviews']])
     recommended_dishes = ''.join([review.get('details', {}).get('recommended_dishes', '') for review in raw_reviews_data['reviews']])
-    print("Recommended Dishes:\n",recommended_dishes)
-    print("\n\n")
-    print("Comments:\n",snippets)
+    # print("Recommended Dishes:\n",recommended_dishes)
+    # print("\n\n")
+    # print("Comments:\n",snippets)
     # Create a DataFrame
-    df = pd.DataFrame({'Restaurant Name': [restaurant_name], 'Reviews': [snippets], 'Recommended Dishes':[recommended_dishes]})
+    # df = pd.DataFrame({'Restaurant Name': [restaurant_name], 'Reviews': [snippets], 'Recommended Dishes':[recommended_dishes]})
+    df = pd.DataFrame({'Restaurant': [restaurant_name], "People's Choice":[recommended_dishes]})
 
-    return df
+    
+    
+    
+    # Process the snippet text
+    processed_text = snippets.split(". ")  # Split the text at each full stop
+
+    # Join the processed text with line breaks
+    display_text = "\n".join(processed_text)
+
+
+
+
+
+
+
+
+    return df,display_text
+
+
+
+def get_map(restuarant_name):
+
+    restuarant_name = restuarant_name
+    params = {
+    "api_key": "YOUR_SERPAPI_KEY",
+    "engine": "google_local",
+    "google_domain": "google.com",
+    "q": restuarant_name
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+
+    # Extract latitude and longitude
+    lat = results['local_results'][0]['gps_coordinates']['latitude']
+    long = results['local_results'][0]['gps_coordinates']['longitude']
+
+    return lat,long
