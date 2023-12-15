2d18c60c9fc038e30e6a0865c25a6b54bc7c024f
Harsh Shah <shah.harsh7@northeastern.edu>
2023-12-15 16:13:22 -0500

    added users table in monitoring

--- streamlit/auth_user.py
+++ streamlit/auth_user.py
@@ -1,6 +1,6 @@
 import sqlite3
 import streamlit as st
-
+import pandas as pd
 
 
 def create_user(name, username, password):
@@ -42,4 +42,29 @@ def login_user(username, password):
 
 
 
+def get_users():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('user.db')
+    cursor = conn.cursor()
+
+    # Execute a query to fetch the table data
+    cursor.execute('SELECT * FROM users')  # Replace 'your_table_name' with the actual table name
+
+    # Fetch all rows from the query result
+    rows = cursor.fetchall()
+
+    # Get the column names
+    columns = [description[0] for description in cursor.description]
+
+    # Create a DataFrame from the fetched data and column names
+    df = pd.DataFrame(rows, columns=columns)
+
+    # Calculate total number of users
+    total_users = len(df)
+
+    # Display the total number of users
+    st.write(f"Total users registered on BiteBuddy: {total_users}")
+
+    # Display the DataFrame using Streamlit
+    st.write(df)
 
